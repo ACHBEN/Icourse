df016e72b0deda81b84b97264ae4974a646f344b
ACHBEN <17190@ECAM.be>
2022-05-27 01:18:28 +0200

    export board

--- UI/icourse/src/components/board/Board.jsx
+++ UI/icourse/src/components/board/Board.jsx
@@ -7,4 +7,11 @@ class Board extends React.Component{
         super(props);
 
     }
+
+    render(){
+        return(
+            <div class="sketch" id="sketch"><canvas className="board" id="board"></canvas></div>
+        )
+    } 
 }
+export default Board
